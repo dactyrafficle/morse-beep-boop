94d2769f9a4b4f49e12278e43ede414f4dd0045f
dactyrafficle <40529986+dactyrafficle@users.noreply.github.com>
2021-09-28 22:28:55 -0400

    updated timing

--- setup.js
+++ setup.js
@@ -18,19 +18,20 @@ const ctx = new (window.AudioContext || window.webkitAudioContext)(); // so this
   
   // mybutton.onclick = e => {
   document.getElementById('mybutton-chars').addEventListener('click', function(e) {
+    
     let time = 0; // a simple time counter
-		var input_text = myinput.value;
-    const sequence = morse.encode(input_text);
-		console.log(input_text);
+		let input_text = myinput.value;
+    
+    const sequence = morse.encode(input_text); //.replace('/',''); // i added the .replace()
+    morse_sequence.innerHTML = sequence.replace('/','<span style="color: transparent;">-</span>') + ' ';
 		
+    console.log(input_text);
+		console.log(sequence);
+    
 		container.innerHTML = '';
-    morse_sequence.innerHTML = '';
-
 
     for (let i = 0; i < input_text.length; i++) {
-      
-      morse_sequence.innerHTML += morse.encode(input_text[i]) + ' ';
-      
+
 			let tr = document.createElement('tr');
       container.appendChild(tr);
       
@@ -44,7 +45,7 @@ const ctx = new (window.AudioContext || window.webkitAudioContext)(); // so this
       (function() {
         let td = document.createElement('td');
         td.classList.add('morse-chars');
-        td.innerHTML = morse.encode(input_text[i]);
+        td.innerHTML = morse.encode(input_text[i]); //.replace('/',''); // i added the .replace()
         tr.appendChild(td);
 			})();
 
@@ -56,15 +57,20 @@ const ctx = new (window.AudioContext || window.webkitAudioContext)(); // so this
 
   // mybutton.onclick = e => {
   document.getElementById('mybutton-audio').addEventListener('click', function(e) {
+    
     let time = 0; // a simple time counter
-		var input_text = myinput.value;
-    const sequence = morse.encode(input_text);
-		console.log(input_text);
+		let input_text = myinput.value;
+    
+    const sequence = morse.encode(input_text); //.replace('/',''); // i added the .replace()
+    morse_sequence.innerHTML = sequence.replace('/','<span style="color: transparent;">-</span>') + ' ';
 		
+    console.log(input_text);
+		console.log(sequence);
+    
 		container.innerHTML = '';
 
     for (let i = 0; i < input_text.length; i++) {
-      
+
 			let tr = document.createElement('tr');
       container.appendChild(tr);
       
@@ -78,17 +84,21 @@ const ctx = new (window.AudioContext || window.webkitAudioContext)(); // so this
       (function() {
         let td = document.createElement('td');
         td.classList.add('morse-chars');
-        td.innerHTML = morse.encode(input_text[i]);
+        td.innerHTML = morse.encode(input_text[i]); //.replace('/',''); // i added the .replace()
         tr.appendChild(td);
 			})();
 
 		}
 		
 		
-    console.log(sequence); // dots and dashes as a string [this is using the library we imported]
+    // console.log(sequence); // dots and dashes as a string [this is using the library we imported]
     sequence.split('').forEach(type => {
       if(type === ' ') { // space => 0.5s of silence
-        time += 0.5;
+        time += 0.4;  // should be the length of a dash
+        return;
+      }
+      if(type === '/') { // space => 0.5s of silence
+        time += 0.2;  // should be the length of a dash, bit shorter because there is an extra space
         return;
       }
       // create an AudioBufferSourceNode
@@ -118,8 +128,8 @@ function fetchBuffers() {
 	// promise.all() of course acceps an array of promises
   return Promise.all(
     [
-      'beep.mp3',
-      'boop.mp3' 
+      'beep2.mp3',
+      'boop3.mp3' 
     ].map(url => fetch(url)
       .then(r => r.arrayBuffer())
       .then(buf => ctx.decodeAudioData(buf))
